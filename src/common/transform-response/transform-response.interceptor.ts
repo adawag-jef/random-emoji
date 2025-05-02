54e56224ab29094eefc0cee9950e122500e1bc6d
jef-adawag <jeff.adawag@netroadshow.com>
2025-05-03 06:23:59 +0800

    feat: add TransformResponseInterceptor to modify response structure and log transformation

---
+++ src/common/transform-response/transform-response.interceptor.ts
@@ -0,0 +1,20 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { map, Observable } from 'rxjs';
+
+@Injectable()
+export class TransformResponseInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      map((data) => {
+        console.log(`Interceptor: transforming response`);
+        return { data };
+      }),
+    );
+  }
+}

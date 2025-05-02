60c6e8ef9fcf4c62c579c755871d51c72952f634
jef-adawag <jeff.adawag@netroadshow.com>
2025-05-03 06:09:51 +0800

    refactor: remove unnecessary eslint-disable comments in EmojiValidationPipe tests

--- src/common/emoji-validation/emoji-validation.pipe.spec.ts
+++ src/common/emoji-validation/emoji-validation.pipe.spec.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { BadRequestException } from '@nestjs/common';
 import { EmojiValidationPipe } from './emoji-validation.pipe';
 
@@ -12,22 +11,18 @@ describe('EmojiValidationPipe', () => {
     expect(result).toBeUndefined();
   });
   it('should throw a bad request error if the value is not a number', () => {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
     const result = () => emojiPipe.transform('not a number');
     expect(result).toThrow(BadRequestException);
   });
   it('should throw a bad request error if the value is less than 0', () => {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
     const result = () => emojiPipe.transform(-1);
     expect(result).toThrow(BadRequestException);
   });
   it('should throw a bad request error if the value is greater than 10', () => {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
     const result = () => emojiPipe.transform(11);
     expect(result).toThrow(BadRequestException);
   });
   it('should return the respective string input as a number', () => {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
     const result = emojiPipe.transform('5');
     expect(result).toEqual(5);
   });

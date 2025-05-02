b8265f88c311d9cd8c52d4d6191dd8f8e43d74be
jef-adawag <jeff.adawag@netroadshow.com>
2025-05-03 06:14:22 +0800

    feat: update getHello method to return browser information along with emoji

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,20 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, Req } from '@nestjs/common';
 import { AppService } from './app.service';
 import { EmojiValidationPipe } from './common/emoji-validation/emoji-validation.pipe';
+import { Request } from 'express';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(@Query('index', EmojiValidationPipe) index?: number): string {
-    console.log(index);
-    return this.appService.getEmoji(index);
+  getHello(
+    @Req() request: Request,
+    @Query('index', EmojiValidationPipe) index?: number,
+  ) {
+    return {
+      emoji: this.appService.getEmoji(index),
+      browser: request.headers.browser,
+    };
   }
 }

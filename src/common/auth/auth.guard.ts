c31efc12c861375b9f15bd15dfd6b66fe2a3c1a0
jef-adawag <jeff.adawag@netroadshow.com>
2025-05-05 22:17:09 +0800

    feat: implement LoggerService and integrate it into AuthGuard for improved logging

--- src/common/auth/auth.guard.ts
+++ src/common/auth/auth.guard.ts
@@ -1,20 +1,23 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Request } from 'express';
 import { Observable } from 'rxjs';
+import { LoggerService } from '../../logger.service';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
+  constructor(private readonly logger: LoggerService) {}
+
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
-    console.log('Guard: checking authentication');
+    this.logger.info('Guard: checking authentication');
     const request: Request = context.switchToHttp().getRequest();
     const apiKey = request.header('x-api-key');
     if (apiKey !== 'SECRET') {
-      console.log('Guard: failed authentication');
+      this.logger.info('Guard: failed authentication');
       return false;
     }
-    console.log('Guard: passed authentication');
+    this.logger.info('Guard: passed authentication');
     return true;
   }
 }
